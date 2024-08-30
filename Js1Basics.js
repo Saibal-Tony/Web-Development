96b3f43f4ca04e86c11f858b86e6f2f3417f9d7f
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-08-30 14:28:05 +0530

    first commit

---
+++ Js1Basics.js
@@ -0,0 +1,22 @@
+alert("Hello World")
+
+console.log("code is running") // this shows how far are you into the code
+console.log("code is also running")
+console.log("code is looking like a wow....")
+
+var a = prompt("Enter your number:")
+var isTrue = confirm("are you sure you want to blast your conputer ?")
+
+if(isTrue)
+{
+    console.log("Computer is Blasting")
+}
+else
+{
+    console.log("Computer is not Blasting")
+}
+
+console.log("The number is " + a)
+
+document.title = "Hey I a good" // changing the title of the page
+document.body.style.backgroundColor = "aqua"
