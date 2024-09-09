bcd049a63f6cd3ae304c16a71bac62b63b85950e
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-09-09 23:00:54 +0530

    changes

---
+++ Js6String.js
@@ -0,0 +1,11 @@
+console.log("String")
+
+let a = "Saibal"
+console.log(a);
+console.log(a[1]); // prints the first character
+
+console.log(a.length);
+
+let friend_1 = "Saibal";
+let friend_2 = "Ayush";
+console.log("My name is " + friend_1 + " and my best friend is " + friend_2);
