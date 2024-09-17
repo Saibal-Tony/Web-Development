770064c3d68df3d53695138ce5dd3f5f59e6f516
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-09-18 00:54:42 +0530

    changes

--- Js10DOM_Insert_remove.js
+++ Js10DOM_Insert_remove.js
@@ -37,4 +37,13 @@ document.querySelector(".box").dataset ;
 let DIV = document.createElement("div"); // DIV - is the empty continer which hold the value ; div - is an empty element created but not seen in DOM until appended
 DIV.innerHTML = "Hi there , nice to meet you <b>Tony</b>";
 DIV.setAttribute("class" , "created"); // class - "created" - made a class name created
-document.querySelector(".container").append(DIV);
+document.querySelector(".container").append(DIV); // we can see this appended at the end 
+document.querySelector(".container").before(DIV); // add before .container
+
+let cont = document.querySelector(".container");
+cont.insertAdjacentHTML("afterward" , "Hey there all") // afterbegin ; afterbegin ; beforeend ; afterend
+
+document.querySelector(".container").classList ; /// lists down all the classes
+document.querySelector(".container").classList.add("tony"); // a new tony class is added
+document.querySelector(".container").classList.remove("tony"); // tony class is removed 
+document.querySelector(".container").classList.toggle("tony"); // toggle - if the class is present then it will be removed , and if not present then added
