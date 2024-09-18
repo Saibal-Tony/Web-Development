0a1a356f0a2ee03e5d702d919c5a92d5bf20f286
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-09-18 23:46:37 +0530

    changes

--- Exercise/Exercise 9/ExerciseJs9.js
+++ Exercise/Exercise 9/ExerciseJs9.js
@@ -2,64 +2,64 @@ console.log("Random Box pattern")
 
 let rand = Math.random();
 
-let e = document.getElementsByClassName("box")
-if(rand < 0.2){
-    e[0].style.backgroundColor = "green"
-    e[0].style.color = "white"
-    e[1].style.backgroundColor = "red"
-    e[1].style.color = "black"
-    e[2].style.backgroundColor = "blue"
-    e[2].style.color = "white"
-    e[3].style.backgroundColor = "aqua"
-    e[3].style.color = "black"
-    e[4].style.backgroundColor = "brown"
-    e[4].style.color = "black"
-}
-if( rand >= 0.2 && rand < 0.4){
-    e[0].style.backgroundColor = "brown"
-    e[0].style.color = "black"
-    e[2].style.backgroundColor = "blue"
-    e[2].style.color = "white"
-    e[1].style.backgroundColor = "aqua"
-    e[1].style.color = "black"
-    e[3].style.backgroundColor = "red"
-    e[3].style.color = "white"
-    e[4].style.backgroundColor = "green"
-    e[4].style.color = "black"
-}
-if( rand >= 0.4 && rand < 0.6){
-    e[0].style.backgroundColor = "blue"
-    e[0].style.color = "black"
-    e[4].style.backgroundColor = "aqua"
-    e[4].style.color = "black"
-    e[0].style.backgroundColor = "red"
-    e[0].style.color = "white"
-    e[2].style.backgroundColor = "green"
-    e[2].style.color = "black"
-    e[3].style.backgroundColor = "brown"
-    e[3].style.color = "black"
-}
-if( rand >= 0.6 && rand < 0.8){
-    e[0].style.backgroundColor = "aqua"
-    e[0].style.color = "black"
-    e[4].style.backgroundColor = "blue"
-    e[4].style.color = "black"
-    e[2].style.backgroundColor = "red"
-    e[2].style.color = "white"
-    e[1].style.backgroundColor = "green"
-    e[1].style.color = "black"
-    e[3].style.backgroundColor = "brown"
-    e[3].style.color = "black"
-}
-if( rand >= 0.8){
-    e[0].style.backgroundColor = "brown"
-    e[0].style.color = "black"
-    e[0].style.backgroundColor = "aqua"
-    e[0].style.color = "black"
-    e[1].style.backgroundColor = "blue"
-    e[1].style.color = "black"
-    e[4].style.backgroundColor = "red"
-    e[4].style.color = "white"
-    e[3].style.backgroundColor = "green"
-    e[3].style.color = "black"
-}
+// let e = document.getElementsByClassName("box")
+// if(rand < 0.2){
+//     e[0].style.backgroundColor = "green"
+//     e[0].style.color = "white"
+//     e[1].style.backgroundColor = "red"
+//     e[1].style.color = "black"
+//     e[2].style.backgroundColor = "blue"
+//     e[2].style.color = "white"
+//     e[3].style.backgroundColor = "aqua"
+//     e[3].style.color = "black"
+//     e[4].style.backgroundColor = "brown"
+//     e[4].style.color = "black"
+// }
+// if( rand >= 0.2 && rand < 0.4){
+//     e[0].style.backgroundColor = "brown"
+//     e[0].style.color = "black"
+//     e[2].style.backgroundColor = "blue"
+//     e[2].style.color = "white"
+//     e[1].style.backgroundColor = "aqua"
+//     e[1].style.color = "black"
+//     e[3].style.backgroundColor = "red"
+//     e[3].style.color = "white"
+//     e[4].style.backgroundColor = "green"
+//     e[4].style.color = "black"
+// }
+// if( rand >= 0.4 && rand < 0.6){
+//     e[0].style.backgroundColor = "blue"
+//     e[0].style.color = "black"
+//     e[4].style.backgroundColor = "aqua"
+//     e[4].style.color = "black"
+//     e[0].style.backgroundColor = "red"
+//     e[0].style.color = "white"
+//     e[2].style.backgroundColor = "green"
+//     e[2].style.color = "black"
+//     e[3].style.backgroundColor = "brown"
+//     e[3].style.color = "black"
+// }
+// if( rand >= 0.6 && rand < 0.8){
+//     e[0].style.backgroundColor = "aqua"
+//     e[0].style.color = "black"
+//     e[4].style.backgroundColor = "blue"
+//     e[4].style.color = "black"
+//     e[2].style.backgroundColor = "red"
+//     e[2].style.color = "white"
+//     e[1].style.backgroundColor = "green"
+//     e[1].style.color = "black"
+//     e[3].style.backgroundColor = "brown"
+//     e[3].style.color = "black"
+// }
+// if( rand >= 0.8){
+//     e[0].style.backgroundColor = "brown"
+//     e[0].style.color = "black"
+//     e[0].style.backgroundColor = "aqua"
+//     e[0].style.color = "black"
+//     e[1].style.backgroundColor = "blue"
+//     e[1].style.color = "black"
+//     e[4].style.backgroundColor = "red"
+//     e[4].style.color = "white"
+//     e[3].style.backgroundColor = "green"
+//     e[3].style.color = "black"
+// }
