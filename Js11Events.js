c9ee1d8922be91e93bf70a7be1d56676c47f969c
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-09-20 23:17:15 +0530

    changes

--- Js11Events.js
+++ Js11Events.js
@@ -1,6 +1,14 @@
 let button = document.getElementById("btn");
 
-button.addEventListener("click", ()=>{
+button.addEventListener("click", ()=>{ // dblclick - gives event after double click
     // alert("I was clicked")
-    document.querySelector("box").innerHTML = "<b>Hi there</b> , nice to be clicked ";
+    document.querySelector(".box").innerHTML = "<b>Hi there</b> , nice to be clicked";
+})
+
+button.addEventListener("contextmenu", ()=>{ // on right clicking the event happens
+    alert("I was clicked")
+})
+
+document.addEventListener("keydown", ()=>{ // any button I press is being registered
+    console.log(e.key) ;
 })
