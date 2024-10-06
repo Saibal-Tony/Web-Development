1a07cee958edfdaae7e1ced959bd13e656a07922
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-10-07 01:01:30 +0530

    changes

---
+++ Java Script/Exercise/Exercise 1/Exercise1.js
@@ -0,0 +1,3 @@
+let get_quantity = 0;
+
+document.querySelector('.quantity').innerText = `The items in the bag are ${get_quantity}.`;
--- Java Script/first.js
+++ Java Script/first.js
@@ -1,1 +1,24 @@
 alert("Hi There");
+
+// arithmetic
+ console.log(1+2);
+
+ //warning message
+ console.warn('this is a virus');
+
+ //error
+ console.error('there is an error in the code');
+
+ // DOM - Documnet object Model
+ document.getElementById('ok'); // getting element by id name 
+
+ document.getElementsByClassName('here') // getting element by class name
+
+ document.querySelector('h1') // getting the element by the tag names
+ document.querySelector('#ok') // getting the element by the id name (#) 
+ document.querySelector('.here') // getting the element by the class name (.)
+ document.querySelectorAll('#ok') // selecting all the element with that particular id 
+
+ document.querySelector('#hi').click(); // button is automatically clicked 
+
+ console.clear(); // clears the console
