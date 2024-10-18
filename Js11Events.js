4563483b525882623b0d6a2b2885a040007d3beb
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-10-18 20:01:08 +0530

    changes

--- Js11Events.js
+++ Js11Events.js
@@ -12,6 +12,26 @@ button.onclock = (evt) => {
     console.log(evt.clientX , evt.clientY ); // prints the position 
 }
 
+let modebtn = document.querySelector("#mode"); // changing the mode from dark to light
+let color = "light";
+
+modebtn.addEventListener("click", () =>{
+    if(color === "light"){
+        color = "dark";
+        // document.querySelector('body').style.backgroundColor = "white" ; 
+        document.querySelector('body').classList.add("dark");
+        document.querySelector('body').classList.remove("light");
+    }
+    else{
+        color = "light"
+        // document.querySelector('body').style.backgroundColor = "black" ; 
+        document.querySelector('body').classList.add("light");
+        document.querySelector('body').classList.remove("dark");
+    }
+
+    console.log(color);
+});
+
 button.addEventListener("click", ()=>{ // dblclick - gives event after double click
     // alert("I was clicked")
     document.querySelector(".box").innerHTML = "<b>Hi there</b> , nice to be clicked";
