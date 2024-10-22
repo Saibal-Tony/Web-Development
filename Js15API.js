5295db369d7ab2c6e5531a2e1253007e002c3445
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-10-22 23:43:03 +0530

    changes

--- Js15API.js
+++ Js15API.js
@@ -1,3 +1,5 @@
 // API - Application Programming Interface
 // it is a middle man which takes some request and sends to the soures and then sends back some information if sources allows it to , it also contains API keys for secuirity purpose 
 // like zomate can't use google maps directly , so if API allows it API shares the recommended data that can be shared to the end user from source
+
+console.log("start")
