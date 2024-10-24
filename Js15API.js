898afe0f8904a7c235d0941f684a9303baffc54d
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-10-25 02:30:14 +0530

    changes

--- Js15API.js
+++ Js15API.js
@@ -2,11 +2,14 @@
 // it is a middle man which takes some request and sends to the soures and then sends back some information if sources allows it to , it also contains API keys for secuirity purpose 
 // like zomate can't use google maps directly , so if API allows it API shares the recommended data that can be shared to the end user from source
 
+// AJAX - Asynchronous Js & XML
+// JSON - Js Object Notation
+
 console.log("start")
 const URL ="https://cat-fact.herokuapp.com"
 
 const getData = async () => {
-    let response = await fetch(URL);
+    let response = await fetch(URL); // get request
     console.log(response);
     console.log(response.status) // gives just the status
 }
